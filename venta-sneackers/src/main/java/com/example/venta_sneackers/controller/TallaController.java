c261caf86ecc083f05424efb475b2f455fc26016
JorgeGuzman-32 <jor.guzmans@duocuc.cl>
2026-06-16 17:41:32 -0400

    Jor.GS 16062026 1740

--- venta-sneackers/src/main/java/com/example/venta_sneackers/controller/TallaController.java
+++ venta-sneackers/src/main/java/com/example/venta_sneackers/controller/TallaController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.example.venta_sneackers.Service.TallaService;
 import com.example.venta_sneackers.dto.TallaResponseDTO;
 
